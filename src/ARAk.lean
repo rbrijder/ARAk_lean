3307924310060a16176557d4428bcd0b01a4372b
Robert Brijder <robert.brijder@uhasselt.be>
2020-08-21 21:04:35 +0200

    make more variables implicit

--- src/ARAk.lean
+++ src/ARAk.lean
@@ -34,10 +34,10 @@ namespace ARA
 section ARA
 -- The goal is to prove the main ARA result
 
-@[reducible] def mut_compat {att: Type} [setoid att] (Y : set att) : Prop := ∀ x y ∈ Y, x ≈ y
-@[reducible] def compat_func {att: Type} [setoid att] {X Y : set att} (φ : X → Y) : Prop := ∀ x : X, (x : att) ≈ ((φ x) : att)
+@[reducible] def mut_compat {att : Type} [setoid att] (Y : set att) : Prop := ∀ x y ∈ Y, x ≈ y
+@[reducible] def compat_func {att : Type} [setoid att] {X Y : set att} (φ : X → Y) : Prop := ∀ x : X, (x : att) ≈ ((φ x) : att)
 
-inductive ARAe (rel att: Type) [setoid att] : Type
+inductive ARAe (rel att : Type) [setoid att] : Type
 | relnm (R : rel) : ARAe
 | union : ARAe → ARAe → ARAe
 | proj (Y : finset att) : ARAe → ARAe
@@ -45,7 +45,7 @@ inductive ARAe (rel att: Type) [setoid att] : Type
 | rename (φ : att → att) (hinj : function.injective φ) (hc : compat_func (set.func_to_univ φ)) : ARAe → ARAe
 | join : ARAe → ARAe → ARAe
 
-parameters {rel att dom: Type} [setoid att]
+parameters {rel att dom : Type} [setoid att]
 
 -- defines schema of an ARA expression
 def ARAschema (e : ARAe rel att) (S : rel → finset att) : finset att :=
@@ -68,12 +68,12 @@ ARAe.rec_on e (λ R, true) -- relnm
 -- def semantics
 def dom_assign := quotient _inst_1 → finset dom -- not assumed that assigned domain is nonempty
 
-variables (D : dom_assign)
-variables (X X' : finset att)
-@[reducible] def tuple := Π (A : (↑X : set att)), (↑(D⟦A⟧) : set dom)
-variables (α : Type) [semiring α]
+@[reducible] def tuple (D : dom_assign) (X : finset att) := Π (A : (↑X : set att)), (↑(D⟦A⟧) : set dom)
+def relation (D : dom_assign) (X : finset att) (α : Type) := tuple D X → α
+def db_instance (D : dom_assign) (S : rel → finset att) (α : Type) := Π (R : rel), relation D (S R) α
 
-def relation (α : Type) := tuple D X → α
+variables {D : dom_assign} {X X' : finset att}
+variables {α : Type} [semiring α]
 
 theorem inclusion_compat {s t : set att} (h : s ⊆ t) : compat_func (set.inclusion h) :=
 begin
@@ -108,13 +108,13 @@ end
 
 def tuple_comp {D : dom_assign} {X X' : finset att} {f : (↑X' : set att) → (↑X : set att)} (t : tuple D X)
 (h : compat_func f) : tuple D X' :=
-(λ A, (tuple_val_change_type D (func_compat_eq_dom h A) (t (f A))))
+(λ A, (tuple_val_change_type (func_compat_eq_dom h A) (t (f A))))
 
 def relation_mut_eq {D : dom_assign} {X : finset att} {Y : set att} (h : mut_compat Y) (t : tuple D X) : Prop :=
 (∀ A B : (↑X) ∩ Y,
 let fil := (set.inclusion (set.inter_subset_left ↑X Y)) in
 let fir := (set.inclusion (set.inter_subset_right ↑X Y)) in
-(tuple_val_change_type D (mut_compat_eq_dom h (fir A) (fir B)) (t (fil A))) = t (fil B))
+(tuple_val_change_type (mut_compat_eq_dom h (fir A) (fir B)) (t (fil A))) = t (fil B))
 
 --set_option pp.implicit true
 --set_option pp.coercions true
@@ -133,18 +133,19 @@ def rel_renaming (r : relation D X α) (φ : att → att) (h : (compat_func (set
 def rel_join (r : relation D X α) (r' : relation D X' α) :
               relation D (X ∪ X') α := (λ t, r(tuple_comp t (inclusion_compat (finset.subset_union_left X X'))) *
                                             r'(tuple_comp t (inclusion_compat (finset.subset_union_right X X'))))
-def db_instance (S : rel → finset att) := Π (R : rel), relation D (S R) α
 
 -- Note: we define semantics without assuming well-typedness. 
 -- This requires rel_union to be defined above in a more general setting.
-def ARA_semantics (e : ARAe rel att) (S : rel → finset att) (I : db_instance D α S) :
+def ARA_semantics (e : ARAe rel att) (S : rel → finset att) (I : db_instance D S α) :
                   relation D (ARAschema e S) α :=
 ARAe.rec_on e I -- relnm
-              (λ e1 e2 e1W e2W, rel_union D (ARAschema e1 S) (ARAschema e2 S) α e1W e2W) -- union
-              (λ Y e1 e1W, rel_proj D (ARAschema e1 S) α e1W Y pi.fintype) -- proj
-              (λ Y hmutc e1 e1W, rel_selection D (ARAschema e1 S) α e1W Y hmutc) -- selection
-              (λ φ hinj hc e1 e1W, rel_renaming D (ARAschema e1 S) α e1W φ hc) -- rename
-              (λ e1 e2 e1W e2W, rel_join D (ARAschema e1 S) (ARAschema e2 S) α e1W e2W) -- join
+              (λ e1 e2 e1W e2W, rel_union e1W e2W) -- union
+              (λ Y e1 e1W, rel_proj e1W Y pi.fintype) -- proj
+              (λ Y hmutc e1 e1W, rel_selection e1W Y hmutc) -- selection
+              (λ φ hinj hc e1 e1W, rel_renaming e1W φ hc) -- rename
+              (λ e1 e2 e1W e2W, rel_join e1W e2W) -- join
+
+--theorem rel_union_comm (r : relation D X α) (r' : relation D X' α) : rel_union r r' = rel_union r' r := sorry
 
 end ARA
 end ARA
